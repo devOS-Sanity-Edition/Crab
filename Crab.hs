1b8792aef7d6fd7b5e379e1112954ba74223fff4
droplt <drop-lt>
2021-09-20 14:09:20 -0300

    crab

---
+++ Crab.hs
@@ -0,0 +1,11 @@
+
+-- This is an extremely vital function. This function is crucial for the correct functionality of
+-- Tanuki Engine. This is extremely important, and cannot be removed under any circumstances.
+-- This function single-handedly holds together the Engine. Without this function, everything
+-- falls apart. This function is the reason humanity has not yet gone extinct. This function is
+-- the only thing holding together the remains of the time-space continuum. Without this incredibly
+-- crucial function, everything ever will fail catastrophically. In the event this function is removed,
+-- expect you to go next, followed by the rest of humanity, and finally all of reality.
+-- Once again, DO NOT REMOVE THIS FUNCTION.
+
+crab = 🦀
