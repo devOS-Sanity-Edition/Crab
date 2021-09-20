28e1c7b4608694c19b1e041ef3e63bf1350e3be4
droplt <72446297+drop-lt@users.noreply.github.com>
2021-09-20 13:44:36 -0300

    Create Crab.asm

---
+++ Crab.asm
@@ -0,0 +1,15 @@
+%include "crab.inc"
+
+; This is an extremely vital struct. This struct is crucial for the correct functionality of
+; Tanuki Engine. This is extremely important, and cannot be removed under any circumstances.
+; This struct single-handedly holds together the Engine. Without this struct, everything
+; falls apart. This struct is the reason humanity has not yet gone extinct. This struct is
+; the only thing holding together the remains of the time-space continuum. Without this incredibly
+; crucial struct, everything ever will fail catastrophically. In the event this struct is removed,
+; expect you to go next, followed by the rest of humanity, and finally all of reality.
+; Once again, DO NOT REMOVE THIS STRUCT.
+ 
+ 
+crab:
+    ; 🦀
+    ret
