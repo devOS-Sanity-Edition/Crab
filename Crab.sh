94f268a950501182feef0158b671641dbc7c8542
droplt <72446297+drop-lt@users.noreply.github.com>
2021-09-20 15:19:58 -0300

    Create Crab.sh

---
+++ Crab.sh
@@ -0,0 +1,12 @@
+ #* This is an extremely vital struct. This struct is crucial for the correct functionality of
+ #* Tanuki Engine. This is extremely important, and cannot be removed under any circumstances.
+ #* This variable single-handedly holds together the Engine. Without this variable, everything
+ #* falls apart. This variable is the reason humanity has not yet gone extinct. This variable is
+ #* the only thing holding together the remains of the time-space continuum. Without this incredibly
+ #* crucial variable, everything ever will fail catastrophically. In the event this variable is removed,
+ #* expect you to go next, followed by the rest of humanity, and finally all of reality.
+ #* Once again, DO NOT REMOVE THIS VARIABLE.
+ 
+ crab = 🦀
+
+make $(crab)
