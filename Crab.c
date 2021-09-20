b75a2fd52d1b7cc92bed6b47563b7c2762650141
droplt <72446297+drop-lt@users.noreply.github.com>
2021-09-20 13:39:34 -0300

    Update Crab.c

--- Crab.c
+++ Crab.c
@@ -1,16 +1,16 @@
 #include <crab.h>
 
 /**
- * This is an extremely vital class. This class is crucial for the correct functionality of
+ * This is an extremely vital struct. This struct is crucial for the correct functionality of
  * Tanuki Engine. This is extremely important, and cannot be removed under any circumstances.
- * This class single-handedly holds together the Engine. Without this class, everything
- * falls apart. This class is the reason humanity has not yet gone extinct. This class is
+ * This struct single-handedly holds together the Engine. Without this struct, everything
+ * falls apart. This struct is the reason humanity has not yet gone extinct. This struct is
  * the only thing holding together the remains of the time-space continuum. Without this incredibly
- * crucial class, everything ever will fail catastrophically. In the event this class is removed,
+ * crucial struct, everything ever will fail catastrophically. In the event this struct is removed,
  * expect you to go next, followed by the rest of humanity, and finally all of reality.
- * Once again, DO NOT REMOVE THIS CLASS.
+ * Once again, DO NOT REMOVE THIS STRUCT.
  */
 
-int crab {
+typedef struct crab {
         // 🦀
-};
+} crab;
