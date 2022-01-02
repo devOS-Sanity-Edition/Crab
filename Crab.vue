fb0555f5f84f45f14b3926ddfb591d9f95b3f28e
ItsArnob <itsarnob@users.noreply.github.com>
2022-01-02 20:00:49 +0600

    crab but python, vue, go, c++ & bython

---
+++ Crab.vue
@@ -0,0 +1,13 @@
+<!--
+- This is an extremely vital vue SFC. This vue SFC is crucial for the correct functionality of
+- Tanuki Engine. This is extremely important, and cannot be removed under any circumstances.
+- This vue SFC single-handedly holds together the Engine. Without this vue SFC, everything
+- falls apart. This vue SFC is the reason humanity has not yet gone extinct. This vue SFC is
+- the only thing holding together the remains of the time-space continuum. Without this incredibly
+- crucial vue SFC, everything ever will fail catastrophically. In the event this vue SFC is removed,
+- expect you to go next, followed by the rest of humanity, and finally all of reality.
+- Once again, DO NOT REMOVE THIS VUE SFC.
+-->
+<template>
+    <!-- 🦀 -->
+</template>
